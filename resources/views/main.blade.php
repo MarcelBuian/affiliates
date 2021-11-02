b52ede38b89676354f211474f25fe4c0732e1f8a
Marcel Buian <marcel.buian@runitonce.com>
2021-11-02 04:45:58 +0100

    Remove the js comments from HTML

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,26 +12,20 @@
     <title>Affiliates</title>
 </head>
 <body>
-<div class="container">
-    <form role="form" method="POST" action="/">
-        @csrf
-        <div class="col-xs-12">
-            @include('form')
-        </div>
-    </form>
-    <hr>
-    <h3>{{ $message }}</h3>
-    <ul class="list-group">
-    @foreach($foundAffiliates as $affiliate /** @var \App\Models\Affiliate $affiliate */)
-        <li class="list-group-item"><strong>ID {{ $affiliate->getId() }}</strong> - {{ $affiliate->getName() }}</li>
-    @endforeach
-    </ul>
-</div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-{{--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>--}}
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>--}}
-{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>--}}
+    <div class="container">
+        <form role="form" method="POST" action="/">
+            @csrf
+            <div class="col-xs-12">
+                @include('form')
+            </div>
+        </form>
+        <hr>
+        <h3>{{ $message }}</h3>
+        <ul class="list-group">
+        @foreach($foundAffiliates as $affiliate /** @var \App\Models\Affiliate $affiliate */)
+            <li class="list-group-item"><strong>ID {{ $affiliate->getId() }}</strong> - {{ $affiliate->getName() }}</li>
+        @endforeach
+        </ul>
+    </div>
 </body>
 </html>
